dbc17b4f54035db543a7cb71f645171ddbbb7fa7
jk0dick <martionkang@outlook.com>
2017-02-10 15:46:00 +0800

    mirgration fix

--- Nucular/migrations/Nucular_1.0.4.lua
+++ Nucular/migrations/Nucular_1.0.4.lua
@@ -1,4 +1,9 @@
 for i, force in pairs(game.forces) do 
  force.reset_recipes()
  force.reset_technologies()
+
+    if force.technologies["particle-physics"].researched then
+        force.recipes["uranium-solid-fuel"].enabled = true
+    end
+
 end
